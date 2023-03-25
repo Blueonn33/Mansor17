7397676f81ce5263dca55cac05d91d7f23ddfcc2
Blueonn33 <91683317+Blueonn33@users.noreply.github.com>
2023-03-25 19:54:09 +0200

    changed the register form

--- Mansor/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Mansor/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -119,6 +119,8 @@ namespace Mansor.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
+                user.Name = Input.Name;
+
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
